6b343e40bc87173d0881a4e8b5c6c098e825cd79
tanmaysharma231 <34827814+tanmaysharma231@users.noreply.github.com>
2023-03-14 12:13:22 +0530

    Add files via upload

---
+++ scraping.py
@@ -0,0 +1,44 @@
+import requests
+from pytube import YouTube
+from pytube import Channel
+import json
+from csv import writer
+import csv
+response = requests.get(
+    url="https://app.scrapingbee.com/api/v1/store/google",
+    params={
+        "api_key": "LZVDH02ZX1JF2QIPTPUCATOKAM0H2SVWSSA1GMYM3F644K6OMZTNWT753H1DUTJB88JF8Y791TO3T1ZH",
+        "search": "site:youtube.com openinapp.co",
+        "nb_results": "10000",
+    },
+
+
+)
+# print(response.content)
+
+obj = json.loads(response.content)
+l1 = obj['organic_results']
+urls = []
+for i in range(len(l1)):
+    t = l1[i].get("url")
+
+    if(t.find('channel') != 0 | t.find('/c') != 0):
+        urls.append(t)
+
+        # print(t)
+    elif('https://www.youtube.com/watch?v=' in t):
+        # print(t)
+        vid = t.strip()
+        x = YouTube(vid)
+        C_url = x.channel_url
+        # print(C_url)
+        urls.append(C_url)
+    elif('https://www.youtube.com/' in t):
+        urls.append(t)
+
+
+writer = open("Listed/Channel_links.csv", 'w+')
+writer_out = csv.writer(writer, delimiter='\n')
+fields = ['channel link']
+writer_out.writerow(fields)
+writer_out.writerow(urls)
